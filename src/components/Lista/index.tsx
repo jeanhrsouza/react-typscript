34ac184d80e9ba9615d895416a21394ee49a6dbc
jean-souza-ey <jeanHenrique.Souza@br.ey.com>
2023-08-01 16:41:37 -0300

    feat(state): adicionando tarefa

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,29 +1,16 @@
-import React, { useState } from "react";
-
+import React from "react";
 import style from './Lista.module.scss';
 import Item from "./Item";
+import { ITarefa } from "../../types/tarefa";
 
-function Lista() {
-    const [tarefas, setTarefas] = useState([
-        {
-            tarefa: 'React',
-            tempo: '02:00:00'
-        },
-        {
-            tarefa: 'JavaScript',
-            tempo: '01:00:00'
-        },
-        {
-            tarefa: 'TypeScript',
-            tempo: '03:00:00'
-        }
-    ]);
+
+
+
+function Lista({ tarefas }: { tarefas: ITarefa[] }) {
+
     return (
         <aside className={style.listaTarefas}>
-            <h2 onClick={() => {
-                console.log('h2 clicado')
-                setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "5:00:00" }])
-            }}>Estudos do dia</h2>
+            <h2>Estudos do dia</h2>
             <ul>
                 {
                     tarefas.map((item, index) => (
